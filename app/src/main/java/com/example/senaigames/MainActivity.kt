05c00251a58cb085814bcdb41c513ee1bc38b490
JENNIFER CRISTINA SANTOS DA SILVA <21276662@CFP127ALUNO.LOCAL>
2022-05-05 16:55:34 -0300

    Finalizando a segunda tela

--- app/src/main/java/com/example/senaigames/MainActivity.kt
+++ app/src/main/java/com/example/senaigames/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.senaigames
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_principal)
+
+        // Arredondando a ImageView
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.profile)
+        val bitmapRound = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        bitmapRound.cornerRadius = 1000f
+        val imgFoto = findViewById<ImageView>(R.id.profile)
+        imgFoto.setImageDrawable(bitmapRound)
+
     }
 }
