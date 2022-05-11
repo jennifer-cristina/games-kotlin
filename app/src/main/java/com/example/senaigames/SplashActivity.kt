bfe077c7a0a8ff94cde92a8505f8f2cb972412cc
Jennifer Cristina <cristin4.jennifer@gmail.com>
2022-05-11 19:32:10 -0300

    Finalizando todas as telas

--- app/src/main/java/com/example/senaigames/SplashActivity.kt
+++ app/src/main/java/com/example/senaigames/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.example.senaigames
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Criar um time para abrir a segunda tela do aplicativo
+        Timer().schedule(2500) { abrirApp() }
     }
+
+    private fun abrirApp() {
+        val intent = Intent(this, principalActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    
 }
--- app/src/main/java/com/example/senaigames/detalhesActivity.kt
+++ app/src/main/java/com/example/senaigames/detalhesActivity.kt
@@ -1,16 +1,11 @@
 package com.example.senaigames
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.util.*
-import kotlin.concurrent.schedule
 
-class principalActivity : AppCompatActivity() {
+class detalhesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
-
-
+        setContentView(R.layout.activity_detalhes)
     }
 }
